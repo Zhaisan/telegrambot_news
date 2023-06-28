a5613222c0dc5fd718231c469d2a2f9e65bdfd5c
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2023-06-28 22:39:48 +0600

    "notifier"

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.20
 
 require (
 	github.com/SlyMarbo/rss v1.0.5
+	github.com/cristalhq/aconfig v0.18.4
+	github.com/cristalhq/aconfig/aconfighcl v0.17.1
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/samber/lo v1.38.1
 	github.com/tomakado/containers v0.0.0-20230620211702-4a5ca7fb9fd3
@@ -11,5 +14,6 @@ require (
 
 require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20230131160201-f062dba9d201 // indirect
 )
--- internal/notifier/notifier.go
+++ internal/notifier/notifier.go
@@ -1,1 +1,28 @@
 package notifier
+
+import (
+	"context"
+	"github.com/Zhaisan/telegrambot_news/internal/model"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+	"time"
+)
+
+type ArticleProvider interface {
+	AllNotPosted(ctx context.Context, since time.Time, limit uint64) ([]model.Article, error)
+	MarkPosted(ctx context.Context, id int64) error
+}
+
+type Summarizer interface {
+	Summarize(ctx context.Context, text string) (string, error)
+}
+
+type Notifier struct {
+	articles         ArticleProvider
+	summarizer       Summarizer
+	bot              *tgbotapi.BotAPI
+	sendInterval     time.Duration
+	lookupTimeWindow time.Duration
+	channelID        int64
+}
